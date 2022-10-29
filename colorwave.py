d87a81beada313e7a7d83945b25ef5992a6f9246
TheSeaUrchin <71513698+TheSeaUrchin@users.noreply.github.com>
2022-10-29 12:14:18 -0400

    Create colorwave.py

---
+++ colorwave.py
@@ -0,0 +1,112 @@
+# Example using PIO to drive a set of WS2812 LEDs.
+
+import array, time
+from machine import Pin
+import rp2
+import colors
+
+# Configure the number of WS2812 LEDs.
+NUM_LEDS = 8
+PIN_NUM = 4
+brightness = 1
+
+@rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
+def ws2812():
+    T1 = 2
+    T2 = 5
+    T3 = 3
+    wrap_target()
+    label("bitloop")
+    out(x, 1)               .side(0)    [T3 - 1]
+    jmp(not_x, "do_zero")   .side(1)    [T1 - 1]
+    jmp("bitloop")          .side(1)    [T2 - 1]
+    label("do_zero")
+    nop()                   .side(0)    [T2 - 1]
+    wrap()
+
+
+# Create the StateMachine with the ws2812 program, outputting on pin
+sm = rp2.StateMachine(0, ws2812, freq=8_000_000, sideset_base=Pin(PIN_NUM))
+
+# Start the StateMachine, it will wait for data on its FIFO.
+sm.active(1)
+
+# Display a pattern on the LEDs via an array of LED RGB values.
+ar = array.array("I", [0 for _ in range(NUM_LEDS)])
+
+##########################################################################
+def pixels_show():
+    dimmer_ar = array.array("I", [0 for _ in range(NUM_LEDS)])
+    for i,c in enumerate(ar):
+        r = int(((c >> 8) & 0xFF) * brightness)
+        g = int(((c >> 16) & 0xFF) * brightness)
+        b = int((c & 0xFF) * brightness)
+        dimmer_ar[i] = (g<<16) + (r<<8) + b
+    sm.put(dimmer_ar, 8)
+    time.sleep_ms(10)
+
+def pixels_set(i, color):
+    ar[i] = (color[1]<<16) + (color[0]<<8) + color[2]
+
+def pixels_fill(color):
+    for i in range(len(ar)):
+        pixels_set(i, color)
+
+def color_chase(color, wait):
+    for i in range(NUM_LEDS):
+        pixels_set(i, color)
+        time.sleep(wait)
+        pixels_show()
+    time.sleep(0.2)
+ 
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        return (0, 0, 0)
+    if pos < 85:
+        return (255 - pos * 3, pos * 3, 0)
+    if pos < 170:
+        pos -= 85
+        return (0, 255 - pos * 3, pos * 3)
+    pos -= 170
+    return (pos * 3, 0, 255 - pos * 3)
+ 
+ 
+def rainbow_cycle(wait,j):
+
+
+        rc_index = (256 // NUM_LEDS) + j
+        pixels_fill(wheel(rc_index & 255))
+        pixels_show()
+        time.sleep(wait)
+        
+        
+def fade_out():
+    global brightness
+    while brightness!=0:
+        brightness =((brightness*10) - 2)/10
+        pixels_show()
+        time.sleep(0.1)
+def fade_in():
+    global brightness
+    brightness = 0
+    while brightness!=1:
+        brightness =((brightness*10) + 2)/10
+        pixels_show()
+        time.sleep(0.1)
+        
+        
+def gradient(first,last):
+
+    pixels_set(0,colors.readColor(first))
+    pixels_set(1,colors.readColor(first))
+    pixels_set(2,colors.readColor(last))
+    pixels_set(3,colors.readColor(last))
+    pixels_show()
+    
+def fill(color):
+    pixels_fill(color)
+    pixels_show()
+
+
